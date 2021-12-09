8d683b5f1abcd943032f991946ec654cc02f3667
Daakmon <Daakmon94@hotmail.es>
2021-12-09 12:31:27 +0100

    Amanda
    Algo

--- app/src/main/java/com/alumno/reto/MainActivity.java
+++ app/src/main/java/com/alumno/reto/MainActivity.java
@@ -12,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //AMD
+//        telefono.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String numero_telefono=telefono.getText().toString();
+//                String url="tel:+34"+numero_telefono;
+//
+//                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                startActivity(intent);
+//            }
+//        });
+//        //hola asiddbaosnfd que tal eo eo eo
+//        //Cambio 2
+//        //Listener Gmail
+//        gmail.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String txt_gmail=gmail.getText().toString();
+//                Toast toast = Toast.makeText(getApplicationContext(), ""+txt_gmail, Toast.LENGTH_LONG);
+//                toast.show();
+//                String url="mailto:"+txt_gmail;
+//                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                startActivity(intent);
+//            }
+//        });
     }
 }
