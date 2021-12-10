44842b2b295e2d6c3b8f526315256bb905e8ca4b
Daakmon <Daakmon94@hotmail.es>
2021-12-10 11:03:36 +0100

    Android
    Tupu

--- app/src/main/java/com/alumno/reto/MainActivity.java
+++ app/src/main/java/com/alumno/reto/MainActivity.java
@@ -2,9 +2,15 @@ package com.alumno.reto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+private Button bCorreo, bTelefono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,29 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //AMD
-//        telefono.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String numero_telefono=telefono.getText().toString();
-//                String url="tel:+34"+numero_telefono;
-//
-//                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//                startActivity(intent);
-//            }
-//        });
-//        //hola asiddbaosnfd que tal eo eo eo
-//        //Cambio 2
-//        //Listener Gmail
-//        gmail.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String txt_gmail=gmail.getText().toString();
-//                Toast toast = Toast.makeText(getApplicationContext(), ""+txt_gmail, Toast.LENGTH_LONG);
-//                toast.show();
-//                String url="mailto:"+txt_gmail;
-//                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//                startActivity(intent);
-//            }
-//        });
+        bCorreo = (Button) findViewById(R.id.ButtonCorreo);
+        bTelefono = (Button) findViewById(R.id.ButtonTelefono);
+
+        bTelefono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numero_telefono=bTelefono.getText().toString();
+                String url="tel:+34"+numero_telefono;
+
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        //Listener Gmail
+        bCorreo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_gmail=bCorreo.getText().toString();
+                Toast toast = Toast.makeText(getApplicationContext(), ""+txt_gmail, Toast.LENGTH_LONG);
+                toast.show();
+                String url="mailto:"+txt_gmail;
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
